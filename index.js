50230626ef794e96c68524a88e9441dddddc158b
liuyuan <liuyuan1992712@qq.com>
2018-04-17 16:19:45 +0800

    update

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import { AppRegistry } from 'react-native';
 // import FetchTestApp from './app/flex_test/FetchNetData';
 // import ParallaxTest from './app/lib_test/ParallaxTest';
 // import ParallaxTest from './app/lib_test/ParallaxTest';
-import NativeBaseTest from './app/nativebase_test/NativeBaseTest';
+// import NativeBaseTest from './app/nativebase_test/NativeBaseTest';
+import ScrollTest from './app/scroll_test/ScrollTest';
 
 // AppRegistry.registerComponent('AwesomeProject', () => App);
 // AppRegistry.registerComponent('reactNativeDemo', () => FlexTestApp);
 // AppRegistry.registerComponent('reactNativeDemo', () => FetchTestApp);
-AppRegistry.registerComponent('reactNativeDemo', () => NativeBaseTest);
+AppRegistry.registerComponent('reactNativeDemo', () => ScrollTest);
